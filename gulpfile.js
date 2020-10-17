da9ec25e48032cd6a22a98c4f19314421bff7de8
Vitaly Kuzmin <vitkuz1337@MacBook-Pro-Vitaly.local>
2020-10-17 13:04:24 +0300

    added test functions

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ let dist = 'dist'
 let source = 'src'
 
 let path = {
-    build: {
+    dist: {
         html: dist + '/',
         css: dist + '/css/',
         js: dist + '/js/',
@@ -17,31 +17,45 @@ let path = {
         fonts: source + '/fonts/*.ttf'
     },
     watch: {
-        html: source + '/**/*.pug',
-        css: source + '/**/*.scss',
-        js: source + '/**/*.js',
-        img: source +'/img/*.{jpg, png, svg, webp}'
+        html: dist + '/**/*.html',
+        css: dist + '/**/*.css',
+        js: dist + '/**/*.js',
+        img: dist +'/img/*.{jpg, png, svg, webp}'
     },
     clean: './' + dist + '/'
 }
 
-let gulp = require('gulp'),
+let gulp = require('gulp'), 
     browsersync = require('browser-sync').create(),
-    pug = require('pug')
+    pug = require('gulp-pug'),
+    htmlValidator = require('gulp-w3c-html-validator')
 
 function browserSync() {
     browsersync.init({
         server: {
             baseDir: './' + dist + '/'    
         },
-        port: 8080,
-        notify: false
+        port: 3000,
+        notify: true
     })
 }
 
-let watch = gulp.parallel(browserSync)
+function pug2html() {
+    return gulp.src('/pages/*.pug')
+        .pipe(pug())
+        .pipe(htmlValidator())
+        .pipe(gulp.dest('/dist/'), browsersync.reload)
+        .pipe(browsersync.stream())
+}
 
+function watchSrc() {
+    gulp.watch([path.watch.html]).on('change', browsersync.reload)
+}
 
+let pugComp = gulp.series(pug2html)
+let watch = gulp.parallel(pugComp, watchSrc, browserSync)
 
+exports.pugComp = pugComp
+exports.pug2html = pug2html
 exports.watch = watch
 exports.default = watch
