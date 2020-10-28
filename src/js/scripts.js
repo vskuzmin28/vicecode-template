db292eb5b555c248c1f7e7707a702b685a90d059
vskuzmin28 <vskuzmin28@gmail.com>
2020-10-28 16:38:28 +0300

    rework scripts

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,21 @@
-$(document).scroll(function () {
+$(document).scroll(() => {
     svg4everybody()
 })
 
+// example scrollTo
+
 $(document).ready(function(){
-    $('.header, footer, .main__tags').on("click","a", function (event) {
-        event.preventDefault();
+    $('.header').on("click","a", (e) => {
+        e.preventDefault()
         let id  = $(this).attr('href'),
             top = $(id).offset().top - 0;
         $('body,html').animate({scrollTop: top}, 1500)
     })
 })
 
-$(document).scroll(function () {
+// exaple header float
+
+$(document).scroll(() => {
     let scroll_status = $(document).scrollTop()
     
     if (scroll_status > $('.header').height()) {
@@ -20,32 +24,4 @@ $(document).scroll(function () {
     else {
       $('.header').removeClass('header--float')
     }
-})
-
-$('.menu').on('click', (e) => {
-    e.preventDefault();
-    
-    $('.menu__default, .menu__active').toggleClass('is-hidden')
-    $('.navigation').toggleClass('is-visible')
-})
-
-$('.navigation__link').on('click', (e) => {
-    e.preventDefault();
-    
-    $('.menu__default, .menu__active').toggleClass('is-hidden')
-    $('.navigation').toggleClass('is-visible')
-})
-
-$('.header__item--phone').on('click', () => {
-    $('.phone-content').toggleClass('is-hidden')
-    $('.dropdown').addClass('is-hidden')
-})
-
-$('.header__item--dropdown').on('click', () => {
-    $('.dropdown').toggleClass('is-hidden')
-    $('.phone-content').addClass('is-hidden')
-})
-
-$('.dropdown__link').on('click', (e) => {
-    $(e.currentTarget).addClass('dropdown__link--active').siblings().removeClass('dropdown__link--active');
 })
