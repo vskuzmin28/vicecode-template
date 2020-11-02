0451388892a6e7acd3382c2057044c5bef85fb8e
vskuzmin28 <vskuzmin28@gmail.com>
2020-11-02 09:13:41 +0300

    added main path domain for task deploy

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,9 +1,10 @@
 const gulp = require('gulp');
 const surge = require('gulp-surge')
+const {website} = require('../../config')
 
 module.exports = function deploy() {
   return surge({
     project: './dist/',
-    domain: '13-1-pro-msi.surge.sh'
+    domain: website
   })
 };
