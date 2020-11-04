b0a6fe6078e7b709ac5a9fc299fcc657b21435a1
vskuzmin28 <vskuzmin28@gmail.com>
2020-11-05 02:21:07 +0300

    clean

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,27 +1,3 @@
 $(document).scroll(() => {
     svg4everybody()
 })
-
-// example scrollTo
-
-$(document).ready(function(){
-    $('.header').on("click","a", (e) => {
-        e.preventDefault()
-        let id  = $(this).attr('href'),
-            top = $(id).offset().top - 0;
-        $('body,html').animate({scrollTop: top}, 1500)
-    })
-})
-
-// exaple header float
-
-$(document).scroll(() => {
-    let scroll_status = $(document).scrollTop()
-    
-    if (scroll_status > $('.header').height()) {
-      $('.header').addClass('header--float')
-    }
-    else {
-      $('.header').removeClass('header--float')
-    }
-})
